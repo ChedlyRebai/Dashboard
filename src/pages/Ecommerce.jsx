9102e3dbc40e9326638854c7dea0e498974aaea9
chedly <chedly.rebai123@gmail.com>
2023-08-21 12:07:18 +0100

    modify width of line chart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -44,8 +44,8 @@ const Ecommerce = () => {
       </div>
 
       <div className="flex gap-10 flex-wrap  mt-1 w-full">
-          <div className="bg-white  dark:text-gray-200 dark:bg-secondary-dark-bg  rounded-2xl  w-full">
-            <div className="flex px-3 justify-between items-center">
+          <div className="bg-white sm:mx-3 dark:text-gray-200 dark:bg-secondary-dark-bg sm:m-3 sm:p-4 rounded-2xl  w-full">
+            <div className="flex justify-between items-center"> 
               <p className="font-semibold text-xl">Revenus</p>
               <div className="flex items-center gap-4">
                 <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
@@ -89,7 +89,7 @@ const Ecommerce = () => {
                 </div> */}
               </div>
               <div className=" ">
-                <LineChart width={screenSize} height={300}/>
+                <LineChart width={screenSize < 500 ? screenSize : 600} height={300}/>
               </div>
             </div>
           </div> 
